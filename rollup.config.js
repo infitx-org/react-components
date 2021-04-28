4ce2ab14a0ca9c4c8095a309314ea30c1f8947c6
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-04-28 16:54:31 +0200

    Export both cjs and esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,74 @@
 import copy from 'rollup-plugin-copy'
 import { babel } from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
 import postcss from 'rollup-plugin-postcss';
-// import resolve from "@rollup/plugin-node-resolve";
+import commonjs from '@rollup/plugin-commonjs';
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
 
+// export default {
+//   input: "./src/components/index.ts",
+//   output: [
+//     {
+//       dir: "lib",
+//       format: "cjs",
+//       sourcemap: true,
+//     },
+//   ],
+//   plugins: [
+//     babel({
+//       babelHelpers: "bundled",
+//     }),
+//     postcss({
+//       config: true,
+//       extract: true,
+//       minimize: true,
+//     }),
+//     peerDepsExternal(),
+//     typescript(),
+//     // commonjs(),
+//     copy({
+//       targets: [
+//         { src: 'src/assets/scss', dest: 'lib' },
+//       ]
+//     })
+//   ],
+// };
+
+
+const packageJson = require("./package.json");
+
 export default {
-  input: "./src/components/index.ts",
+  input: "src/components/index.ts",
   output: [
     {
-      dir: "lib",
+      file: packageJson.main,
       format: "cjs",
-      sourcemap: true,
+      sourcemap: true
     },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true
+    }
   ],
   plugins: [
+    peerDepsExternal(),
     babel({
       babelHelpers: "bundled",
     }),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
     postcss({
       config: true,
       extract: true,
       minimize: true,
     }),
-    peerDepsExternal(),
-    // resolve(),
-    typescript(),
     copy({
       targets: [
         { src: 'src/assets/scss', dest: 'lib' },
       ]
     })
-  ],
+  ]
 };
