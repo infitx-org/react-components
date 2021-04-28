c063ad5ffd43649148bb10788c6aecda82eef428
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-04-28 18:02:02 +0200

    v0.1.1-1

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,10 @@
 import copy from 'rollup-plugin-copy'
 import { babel } from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
-import postcss from 'rollup-plugin-postcss';
 import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
-
-// export default {
-//   input: "./src/components/index.ts",
-//   output: [
-//     {
-//       dir: "lib",
-//       format: "cjs",
-//       sourcemap: true,
-//     },
-//   ],
-//   plugins: [
-//     babel({
-//       babelHelpers: "bundled",
-//     }),
-//     postcss({
-//       config: true,
-//       extract: true,
-//       minimize: true,
-//     }),
-//     peerDepsExternal(),
-//     typescript(),
-//     // commonjs(),
-//     copy({
-//       targets: [
-//         { src: 'src/assets/scss', dest: 'lib' },
-//       ]
-//     })
-//   ],
-// };
-
 
 const packageJson = require("./package.json");
 
@@ -64,6 +34,7 @@ export default {
       config: true,
       extract: true,
       minimize: true,
+      namedExports: true
     }),
     copy({
       targets: [
