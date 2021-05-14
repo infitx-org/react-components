f85b7088464b0a00a350b65b10af0c7099c384f6
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-05-14 19:32:30 +0200

    Make jest resolving absolute paths

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   moduleFileExtensions: ["tsx", "ts", "js", "json"],
+  moduleDirectories: ['node_modules', 'src'],
   roots: ["src"],
   "moduleNameMapper": {
       "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
