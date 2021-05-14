3068abc669c4dbf1b125bce6998af433da8ffcb3
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-05-14 14:56:49 +0200

    Test

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
 export * from "./types";
 
 export { default as Button, ButtonProps } from "./Button";
-export { default as Checkbox, CheckboxProps } from "./Checkbox";
 export { default as DatePicker, DatePickerProps } from "./DatePicker";
+export { default as Checkbox, CheckboxProps } from "./Checkbox";
 export { default as Field, FieldProps } from "./Field";
 export { default as Icon, IconProps } from "./Icon";
 export { default as RadioGroup, RadioGroupProps } from "./RadioGroup";
