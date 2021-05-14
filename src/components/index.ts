4426f739861b6d2a7dfc434d9182bd9143adc457
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-05-14 14:48:21 +0200

    Update tests

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,13 @@
+export * from "./types";
+
 export { default as Button, ButtonProps } from "./Button";
-export { default as Spinner, SpinnerProps } from "./Spinner";
+export { default as Checkbox, CheckboxProps } from "./Checkbox";
+export { default as DatePicker, DatePickerProps } from "./DatePicker";
+export { default as Field, FieldProps } from "./Field";
 export { default as Icon, IconProps } from "./Icon";
 export { default as RadioGroup, RadioGroupProps } from "./RadioGroup";
-export { default as Checkbox, CheckboxProps } from "./Checkbox";
+export { default as Select, SelectProps } from "./Select";
+export { default as Spinner, SpinnerProps } from "./Spinner";
 export {
   Tab,
   Tabs,
@@ -11,6 +16,3 @@ export {
   TabProps,
   TabsProps,
 } from "./Tabs";
-export { default as Select, SelectProps } from "./Select";
-export { default as DatePicker, DatePickerProps } from "./DatePicker";
-export * from "./types";
--- src/components/utils/mergeRefs.ts
+++ src/components/utils/mergeRefs.ts
@@ -1,9 +1,10 @@
 import { ForwardedRef } from "react";
 
-type InputRef<T> = ForwardedRef<T>;
 type FuncRef<T> = (node: T) => void;
 
-export default function mergeRefs<T>(...refs: (InputRef<T> | FuncRef<T>)[]) {
+export default function mergeRefs<T>(
+  ...refs: (ForwardedRef<T> | FuncRef<T>)[]
+) {
   const filteredRefs = refs.filter(Boolean);
   if (!filteredRefs.length) return null;
   if (filteredRefs.length === 0) return filteredRefs[0];
