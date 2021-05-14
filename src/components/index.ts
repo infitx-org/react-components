c29b90b12461c7ca82c970c21704473701354eb9
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-05-14 16:38:06 +0200

    exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,14 @@ export * from "./types";
 export { default as Button, ButtonProps } from "./Button";
 export { default as Checkbox, CheckboxProps } from "./Checkbox";
 // export { default as DatePicker, DatePickerProps } from "./DatePicker";
-export { default as Field, FieldProps } from "./Field";
+export {
+  default as Field,
+  FieldProps,
+  Loader,
+  LoaderProps,
+  Placeholder,
+  PlaceholderProps,
+} from "./Field";
 export { default as Icon, IconProps } from "./Icon";
 export { default as RadioGroup, RadioGroupProps } from "./RadioGroup";
 export { default as Select, SelectProps } from "./Select";
