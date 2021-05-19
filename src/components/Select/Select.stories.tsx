424c1d475ed87fb29c4eda2eff950b1fde3ae883
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-05-19 16:46:45 +0200

    Remove test stories

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -6,7 +6,7 @@ export default {
 };
 
 /* eslint-disable no-console */
-const options = new Array(10).fill(0).map((_, index: number) => ({
+const options = new Array(3).fill(0).map((_, index: number) => ({
   label: index + 1,
   value: index + 1,
 }));
@@ -76,53 +76,3 @@ export const OnClear = Template.bind({});
 OnClear.args = {
   onClear: console.log,
 };
-
-// export const SelectNoState = () => {
-//   const ref = React.useRef<HTMLInputElement>(null);
-//   React.useEffect(() => {
-//     if (ref.current) {
-//       setTimeout(() => ref.current?.focus(), 100);
-//     }
-//   }, [ref.current]);
-
-//   return (
-//     <Select
-//       invalid
-//       options={options}
-//       pending
-//       ref={ref}
-//       required
-//       onFocus={console.log}
-//       onBlur={console.log}
-//     />
-//   );
-// };
-
-// export const SelectValue = () => {
-//   const [value, setValue] = React.useState<string | number | boolean>(
-//     options[0].value
-//   );
-//   return (
-//     <>
-//       <div style={{ height: "200px" }} />
-//       <div
-//         style={{
-//           height: "300px",
-//           background: "#eee",
-//           overflow: "hidden",
-//           paddingTop: "150px",
-//         }}
-//       >
-//         <Select
-//           placeholder="try"
-//           options={options}
-//           value={value}
-//           onChange={setValue}
-//           required
-//           onClear={console.log}
-//         />
-//       </div>
-//       <div style={{ height: "200px" }} />
-//     </>
-//   );
-// };
