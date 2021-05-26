d40facc9dc5055f6e695e7cf9b6ac0473c3639d7
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-05-26 11:30:55 +0200

    Style prop

--- src/components/Pill/Pill.tsx
+++ src/components/Pill/Pill.tsx
@@ -6,6 +6,7 @@ import "./Pill.scss";
 interface PillProps {
   id?: string;
   className?: string;
+  style?: React.CSSProperties;
   kind?: `${Kind}` | "default";
   icon?: React.ReactElement<React.SVGProps<SVGSVGElement>>;
   fill?: string;
@@ -17,6 +18,7 @@ interface PillProps {
 function Pill({
   id,
   className,
+  style,
   kind = "default",
   icon,
   fill = "inherit",
@@ -33,7 +35,7 @@ function Pill({
   ]);
 
   return (
-    <div className={pillClassName} id={id}>
+    <div className={pillClassName} id={id} style={style}>
       {icon && (
         <div className="rc-pill__icon-box">
           <Icon icon={icon} size={16} className="rc-pill__icon" fill={fill} />
