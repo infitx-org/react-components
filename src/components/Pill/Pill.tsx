e54a146f826084304e86b0f616c75e962e436172
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-05-26 11:09:08 +0200

    Pill and stories and tests

---
+++ src/components/Pill/Pill.tsx
@@ -0,0 +1,44 @@
+import classnames from "classnames";
+import { Kind } from "types";
+import Icon from "components/Icon";
+import "./Pill.scss";
+
+interface PillProps {
+  id?: string;
+  className?: string;
+  kind?: `${Kind}` | "default";
+  icon?: React.ReactElement<React.SVGProps<SVGSVGElement>>;
+  fill?: string;
+  label?: string;
+  active?: boolean;
+}
+
+function Pill({
+  id,
+  className,
+  kind = "default",
+  icon,
+  fill = "inherit",
+  label,
+  active,
+}: PillProps) {
+  const pillClassName = classnames([
+    "rc-pill",
+    `rc-pill--${kind}`,
+    active && `rc-pill--active`,
+    className,
+  ]);
+
+  return (
+    <div className={pillClassName} id={id}>
+      {icon && (
+        <div className="rc-pill__icon-box">
+          <Icon icon={icon} size={16} className="rc-pill__icon" fill={fill} />
+        </div>
+      )}
+      {label && <span className="rc-pill__label">{label}</span>}
+    </div>
+  );
+}
+
+export default Pill;
