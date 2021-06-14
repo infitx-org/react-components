013408ad1ba838d47293fe15e1b48f3edd62df23
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-06-14 18:10:40 +0200

    replace icons with test icons

--- src/components/IconButton/IconButton.spec.tsx
+++ src/components/IconButton/IconButton.spec.tsx
@@ -1,16 +1,12 @@
 import { fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import userEvent from "@testing-library/user-event";
+import TestIcon from "resources/icons/test.svg";
 import { Kind } from "types";
 import IconButton from "./IconButton";
 
 const { log } = console;
 
-const icon = (
-  <svg width="55" height="55" viewBox="-6 -3 55 55">
-    <circle cx="10" cy="10" r="20" />
-  </svg>
-);
+const icon = <TestIcon />;
 
 describe("tests the IconButton props", () => {
   it("renders the IconButton", () => {
