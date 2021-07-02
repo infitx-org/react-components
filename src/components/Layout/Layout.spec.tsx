52f3aa838f74ca26de9cc29ec9c5199163ec4cba
Ivan Annovazzi <ivan@modusbox.com>
2021-07-02 17:05:12 +0200

    navbar composition (#66)
    * prepare split components
    
    * Split navbar in compoennts and add overlay
    
    * Block
    
    * Make it work better
    
    * improve block item logic
    
    * Lint fix
    
    * Snapshot
    
    * Tests for the navbar

--- src/components/Layout/Layout.spec.tsx
+++ src/components/Layout/Layout.spec.tsx
@@ -1,6 +1,27 @@
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
 import Layout from "./Layout";
+import TestIcon from "../../resources/icons/test.svg";
+
+describe("tests the Layout as a whole", () => {
+  it("renders the layout components", () => {
+    const { container } = render(
+      <Layout>
+        <Layout.Navbar title="Layout" username="user" />
+        <Layout.Content>
+          <Layout.SideMenu>Menu</Layout.SideMenu>
+          <Layout.Page>Page</Layout.Page>
+        </Layout.Content>
+      </Layout>
+    );
+    expect(container.querySelector(".rc-layout")).toBeTruthy();
+    expect(container.querySelector(".rc-layout__navbar")).toBeTruthy();
+    expect(container.querySelector(".rc-layout__content")).toBeTruthy();
+    expect(container.querySelector(".rc-layout__side-menu")).toBeTruthy();
+    expect(container.querySelector(".rc-layout__page")).toBeTruthy();
+  });
+});
 
 describe("tests the Layout component", () => {
   it("renders the layout", () => {
@@ -12,40 +33,6 @@ describe("tests the Layout component", () => {
   it("renders the prop classname on the layout", () => {
     const { container } = render(<Layout className="test">content</Layout>);
     expect(container.querySelector(".rc-layout.test")).toBeTruthy();
-  });
-});
-
-describe("tests the Layout.Navbar component", () => {
-  it("renders the navbar", () => {
-    const { container } = render(
-      <Layout.Navbar title="Navbar" username="user" />
-    );
-    expect(container.querySelector(".rc-layout__navbar")).toBeTruthy();
-  });
-
-  it("renders the title on the navbar", () => {
-    const { container } = render(
-      <Layout.Navbar title="Navbar" username="user" />
-    );
-    expect(
-      container.querySelector(".rc-layout__navbar__link")?.textContent
-    ).toBe("Navbar");
-  });
-
-  it("renders the username on the navbar", () => {
-    const { container } = render(
-      <Layout.Navbar title="Navbar" username="User" />
-    );
-    expect(
-      container.querySelector(".rc-layout__navbar__user")?.textContent
-    ).toBe("User");
-  });
-
-  it("renders the prop classname on the navbar", () => {
-    const { container } = render(
-      <Layout.Navbar title="Layout" username="user" className="test" />
-    );
-    expect(container.querySelector(".rc-layout__navbar.test")).toBeTruthy();
   });
 });
 
@@ -100,26 +87,106 @@ describe("tests the Layout.Page component", () => {
   });
 });
 
-describe("tests the Layout as a whole", () => {
-  it("renders the layout components", () => {
+describe("tests the Layout.Navbar component", () => {
+  it("renders the navbar", () => {
     const { container } = render(
-      <Layout>
-        <Layout.Navbar title="Layout" username="user" />
-        <Layout.Content>
-          <Layout.SideMenu>Menu</Layout.SideMenu>
-          <Layout.Page>Page</Layout.Page>
-        </Layout.Content>
-      </Layout>
+      <Layout.Navbar title="Navbar" username="user" />
     );
-    expect(container.querySelector(".rc-layout")).toBeTruthy();
     expect(container.querySelector(".rc-layout__navbar")).toBeTruthy();
-    expect(container.querySelector(".rc-layout__content")).toBeTruthy();
-    expect(container.querySelector(".rc-layout__side-menu")).toBeTruthy();
-    expect(container.querySelector(".rc-layout__page")).toBeTruthy();
+  });
+
+  it("renders the title on the navbar", () => {
+    const { container } = render(
+      <Layout.Navbar title="Navbar" username="user" />
+    );
+    expect(
+      container.querySelector(".rc-layout__navbar__link")?.textContent
+    ).toBe("Navbar");
+  });
+
+  it("renders the username on the navbar", () => {
+    const { container } = render(
+      <Layout.Navbar title="Navbar" username="User" />
+    );
+    expect(
+      container.querySelector(".rc-layout__navbar__block")?.textContent
+    ).toBe("User");
+  });
+
+  it("renders the prop classname on the navbar", () => {
+    const { container } = render(
+      <Layout.Navbar title="Layout" username="user" className="test" />
+    );
+    expect(container.querySelector(".rc-layout__navbar.test")).toBeTruthy();
+  });
+
+  describe("tests te Layout.Navbar.Block", () => {
+    it("renders the block", () => {
+      const { container } = render(
+        <Layout.Navbar.Block label="Block" initial="B" />
+      );
+      expect(container.querySelector(".rc-layout__navbar__block")).toBeTruthy();
+      expect(
+        container.querySelector(".rc-layout__navbar__block__label")
+      ).toBeTruthy();
+      expect(
+        container.querySelector(".rc-layout__navbar__block__label")
+      ).toHaveTextContent("Block");
+      expect(
+        container.querySelector(".rc-layout__navbar__block__initial")
+      ).toHaveTextContent("B");
+    });
+
+    it("renders the block icon ", () => {
+      const { container } = render(
+        <Layout.Navbar.Block label="Block" icon={<TestIcon />} />
+      );
+      expect(
+        container.querySelector(".rc-layout__navbar__block__icon svg")
+      ).toBeTruthy();
+    });
+
+    it("renders the overlay items when clicking", () => {
+      const { container } = render(
+        <Layout.Navbar.Block label="Block">
+          <Layout.Navbar.Block.Item label="Item" />
+          <Layout.Navbar.Block.Item label="Item" />
+        </Layout.Navbar.Block>
+      );
+
+      expect(
+        container.querySelectorAll(".rc-layout__navbar__block__item")
+      ).toHaveLength(0);
+
+      userEvent.click(
+        container.querySelector(".rc-layout__navbar__block") as HTMLElement
+      );
+      expect(
+        container.querySelectorAll(".rc-layout__navbar__block__item")
+      ).toHaveLength(2);
+    });
+
+    it("triggers the item onClick function prop when clicked", () => {
+      const mockFn = jest.fn();
+      const { container } = render(
+        <Layout.Navbar.Block label="Block">
+          <Layout.Navbar.Block.Item onClick={mockFn} label="Item" />
+          <Layout.Navbar.Block.Item label="Item" />
+        </Layout.Navbar.Block>
+      );
+
+      userEvent.click(
+        container.querySelector(".rc-layout__navbar__block") as HTMLElement
+      );
+      const block = container
+        .querySelectorAll(".rc-layout__navbar__block__item")
+        .item(0);
+      userEvent.click(block);
+      expect(mockFn).toHaveBeenCalledTimes(1);
+    });
   });
 });
 
-// // Snapshot testing
 it("renders the layout correctly when multiple props are set", () => {
   const { container } = render(
     <Layout>
---
+++ src/components/Layout/components/Navbar/components/Block.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import Icon from "components/Icon";
+import useOnClickOutside from "hooks/useOnClickOutside";
+import Overlay from "./Overlay";
+
+interface BlockItemProps {
+  label?: string;
+  children?: React.ReactNode;
+  onClick?: () => void;
+}
+
+function BlockItem({ label, children, onClick }: BlockItemProps) {
+  return (
+    <div
+      className="rc-layout__navbar__block__item"
+      onClick={onClick}
+      role="presentation"
+    >
+      {label || children}
+    </div>
+  );
+}
+
+function isBlockItem(child: React.ReactNode): boolean {
+  return (child as React.ReactElement).type === BlockItem;
+}
+
+function getBlockItems(
+  children: React.ReactNode,
+  { onClick }: Partial<BlockItemProps>
+) {
+  const childrenArray = React.Children.toArray(children).filter(
+    isBlockItem
+  ) as React.ReactElement<BlockItemProps>[];
+
+  return childrenArray.map((blockItem) =>
+    React.cloneElement(blockItem, {
+      ...blockItem.props,
+      // merge the own click function with the outside one to close the overlay
+      onClick: () => {
+        onClick?.();
+        blockItem.props.onClick?.();
+      },
+    })
+  );
+}
+
+export interface BlockProps {
+  icon?: React.ReactElement<React.SVGProps<SVGSVGElement>>;
+  initial?: string;
+  label?: string;
+  onClick?: () => void;
+  children?: React.ReactNode;
+}
+
+function Block({ icon, initial, label, onClick, children }: BlockProps) {
+  const blockRef = React.useRef<HTMLDivElement>(null);
+  const [overlayVisible, setOverlayVisible] = React.useState(false);
+  const onOverlayToggleClick = () => setOverlayVisible(!overlayVisible);
+  useOnClickOutside(blockRef, () => setOverlayVisible(false));
+
+  let iconContent = null;
+  if (icon) {
+    iconContent = (
+      <div className="rc-layout__navbar__block__icon">
+        <Icon icon={icon} size={30} />
+      </div>
+    );
+  } else if (initial) {
+    iconContent = (
+      <div className="rc-layout__navbar__block__initial">
+        {initial.charAt(0)}
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className="rc-layout__navbar__block"
+      ref={blockRef}
+      onClick={onClick || onOverlayToggleClick}
+      role="presentation"
+    >
+      {iconContent}
+      <div className="rc-layout__navbar__block__label">
+        <span>{label}</span>
+      </div>
+      {children && overlayVisible && (
+        <Overlay>
+          {getBlockItems(children, { onClick: () => setOverlayVisible(false) })}
+        </Overlay>
+      )}
+    </div>
+  );
+}
+
+Block.Item = BlockItem;
+
+export default Block;
