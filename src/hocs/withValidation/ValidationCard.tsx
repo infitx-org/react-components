719d8da06ec96c1f796c0aeb2a918f0e37369de9
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-07-26 15:49:19 +0200

    Rename classnames to avoid conflicts

--- src/hocs/withValidation/ValidationCard.tsx
+++ src/hocs/withValidation/ValidationCard.tsx
@@ -7,15 +7,15 @@ import X from "bootstrap-icons/icons/x.svg";
 import "./ValidationCard.scss";
 
 const ActiveValidationIcon = (
-  <Icon className="validation__message__icon" icon={<X />} size={20} />
+  <Icon className="rc-validation__message__icon" icon={<X />} size={20} />
 );
 
 const InactiveValidationIcon = (
-  <Icon className="validation__message__icon" icon={<Check />} size={20} />
+  <Icon className="rc-validation__message__icon" icon={<Check />} size={20} />
 );
 
 const UnsetValidationIcon = (
-  <div className="validation__message__icon--unset" />
+  <div className="rc-validation__message__icon--unset" />
 );
 
 interface ValidationIconProps {
@@ -34,17 +34,17 @@ const ValidationIcon = ({ active }: ValidationIconProps) => {
 
 const ValidationMessageRow = ({ message, active }: ValidationMessage) => {
   const className = classnames([
-    "validation__message",
-    active && "validation__message--invalid",
-    active === false && "validation__message--valid",
+    "rc-validation__message",
+    active && "rc-validation__message--invalid",
+    active === false && "rc-validation__message--valid",
   ]);
   return (
     <Row>
       <li className={className}>
-        <div className="validation__message__icon-container">
+        <div className="rc-validation__message__icon-container">
           <ValidationIcon active={active} />
         </div>
-        <span className="validation__message__text">{message}</span>
+        <span className="rc-validation__message__text">{message}</span>
       </li>
     </Row>
   );
@@ -62,15 +62,15 @@ export default function ValidationCard({
   required,
   invalid,
 }: ValidationCardProps) {
-  const unknown =
+  const unset =
     !required && messages.every(({ active }) => active === undefined);
   const success = messages.every(({ active }) => active === false);
   const className = classnames([
-    "validation__messages",
-    required && "validation__messages--required",
-    invalid && "validation__messages--invalid",
-    success && "validation__messages--valid",
-    unknown && "validation__messages--unknown",
+    "rc-validation__messages",
+    required && "rc-validation__messages--required",
+    invalid && "rc-validation__messages--invalid",
+    success && "rc-validation__messages--valid",
+    unset && "rc-validation__messages--unset",
   ]);
   return (
     <div>
