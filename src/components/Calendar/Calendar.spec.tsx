cdce307119ce8c0c8a17006b0186b403d60b7909
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-07-28 13:52:08 +0200

    Month and year selection tests

--- src/components/Calendar/Calendar.spec.tsx
+++ src/components/Calendar/Calendar.spec.tsx
@@ -9,13 +9,10 @@ function getDay(
 ): Element | undefined {
   const days = container.querySelectorAll(".rc-calendar__day__cell");
   const dayCell = Array.from(days).find((d) => d.textContent === day);
-  if (!dayCell) {
-    return undefined;
-  }
   if (cell) {
     return dayCell;
   }
-  return dayCell.querySelector(".rc-calendar__day") as Element;
+  return dayCell?.querySelector(".rc-calendar__day") as Element;
 }
 
 describe("tests the Calendar", () => {
@@ -31,7 +28,7 @@ describe("tests the Calendar", () => {
   // Find a way to do that!
   // });
 
-  it("Sets the correct Initial Year", () => {
+  it("Sets the correct initialYear prop", () => {
     const { container } = render(
       <Calendar initialMonth={0} initialYear={2000} />
     );
@@ -40,7 +37,7 @@ describe("tests the Calendar", () => {
     ).toHaveTextContent("2000");
   });
 
-  it("Sets the correct Initial Month", () => {
+  it("Sets the correct initialMonth prop", () => {
     const { container } = render(
       <Calendar initialYear={2021} initialMonth={0} />
     );
@@ -145,5 +142,103 @@ describe("tests the Calendar", () => {
     expect(onRangeClickMock).toHaveBeenCalledTimes(2);
     expect(onRangeClickMock).toHaveBeenCalledWith([startDate, undefined]);
     expect(onRangeClickMock).toHaveBeenCalledWith([startDate, endDate]);
+  });
+});
+
+describe("Tests the calendar year selection", () => {
+  it("Sets the prev year clicking on the prev year icon button", () => {
+    const { container } = render(
+      <Calendar initialYear={2021} initialMonth={0} />
+    );
+
+    userEvent.click(
+      container.querySelector(".rc-calendar__year-prev") as Element
+    );
+    expect(
+      container.querySelector(".rc-calendar__current-year")
+    ).toHaveTextContent("2020");
+  });
+
+  it("Sets the next year clicking on the next year icon button", () => {
+    const { container } = render(
+      <Calendar initialYear={2021} initialMonth={0} />
+    );
+
+    userEvent.click(
+      container.querySelector(".rc-calendar__year-next") as Element
+    );
+    expect(
+      container.querySelector(".rc-calendar__current-year")
+    ).toHaveTextContent("2022");
+  });
+});
+
+describe("Tests the calendar month selection", () => {
+  it("Sets the prev month clicking on the prev month icon button", () => {
+    const { container } = render(
+      <Calendar initialYear={2021} initialMonth={0} />
+    );
+
+    userEvent.click(
+      container.querySelector(".rc-calendar__month-prev") as Element
+    );
+    expect(
+      container.querySelector(".rc-calendar__current-month")
+    ).toHaveTextContent("December");
+  });
+
+  it("Sets the next month clicking on the next month icon button", () => {
+    const { container } = render(
+      <Calendar initialYear={2021} initialMonth={0} />
+    );
+
+    userEvent.click(
+      container.querySelector(".rc-calendar__month-next") as Element
+    );
+    expect(
+      container.querySelector(".rc-calendar__current-month")
+    ).toHaveTextContent("February");
+  });
+
+  it("Opens the month selection when clicking on the month name", () => {
+    const { container } = render(
+      <Calendar initialYear={2021} initialMonth={0} />
+    );
+
+    userEvent.click(
+      container.querySelector(".rc-calendar__current-month") as Element
+    );
+    const months = container.querySelectorAll(".rc-calendar__month");
+    expect(months).toHaveLength(12);
+  });
+
+  it("Closes the month selection when clicking on the month name twice", () => {
+    const { container } = render(
+      <Calendar initialYear={2021} initialMonth={0} />
+    );
+
+    userEvent.click(
+      container.querySelector(".rc-calendar__current-month") as Element
+    );
+    userEvent.click(
+      container.querySelector(".rc-calendar__current-month") as Element
+    );
+    const months = container.querySelectorAll(".rc-calendar__month");
+    expect(months).not.toHaveLength(12);
+    expect(container.querySelectorAll(".rc-calendar__day")).toHaveLength(31);
+  });
+
+  it("Changes the current month when clicking on a month in the month picker", () => {
+    const { container } = render(
+      <Calendar initialYear={2021} initialMonth={0} />
+    );
+
+    userEvent.click(
+      container.querySelector(".rc-calendar__current-month") as Element
+    );
+    const months = container.querySelectorAll(".rc-calendar__month");
+    userEvent.click(months[3] as Element);
+    const currentMonth = container.querySelector(".rc-calendar__current-month");
+    expect(currentMonth).toHaveTextContent("April");
   });
 });
