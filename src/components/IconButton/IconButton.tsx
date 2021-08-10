1513e9b5ef1f81d45ab67a4234b643b7a19ec182
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-08-10 16:19:43 +0200

    Try different ways - no luck

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,5 +1,5 @@
 import classnames from "classnames";
-import React from "react";
+import React, { forwardRef, ForwardedRef } from "react";
 import Icon from "components/Icon";
 import withTooltip from "hocs/withTooltip";
 import { Kind } from "../../types";
@@ -15,43 +15,45 @@ export interface IconButtonProps {
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const IconButton = React.forwardRef(function IconButton(
-  {
-    kind,
-    size = 20,
-    icon,
-    fill,
-    className,
-    onClick,
-    disabled,
-  }: IconButtonProps,
-  forwardedRef: React.ForwardedRef<HTMLButtonElement>
-) {
-  const buttonClassName = classnames([
-    "rc-icon-button",
-    kind && `rc-icon-button--${kind}`,
-    !kind && `rc-icon-button--default`,
-    disabled && "rc-icon-button--disabled",
-    className,
-  ]);
+const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  function IconButton(
+    {
+      kind,
+      size = 20,
+      icon,
+      fill,
+      className,
+      onClick,
+      disabled,
+    }: IconButtonProps,
+    forwardedRef: ForwardedRef<HTMLButtonElement>
+  ) {
+    const buttonClassName = classnames([
+      "rc-icon-button",
+      kind && `rc-icon-button--${kind}`,
+      !kind && `rc-icon-button--default`,
+      disabled && "rc-icon-button--disabled",
+      className,
+    ]);
 
-  return (
-    <button
-      type="button"
-      ref={forwardedRef}
-      className={buttonClassName}
-      onClick={disabled ? undefined : onClick}
-      style={{ height: size, width: size }}
-    >
-      <Icon
-        size={size}
-        icon={icon}
-        fill={fill}
-        style={{ fill: fill || "inherit" }}
-        className="rc-icon-button__icon"
-      />
-    </button>
-  );
-});
+    return (
+      <button
+        type="button"
+        ref={forwardedRef}
+        className={buttonClassName}
+        onClick={disabled ? undefined : onClick}
+        style={{ height: size, width: size }}
+      >
+        <Icon
+          size={size}
+          icon={icon}
+          fill={fill}
+          style={{ fill: fill || "inherit" }}
+          className="rc-icon-button__icon"
+        />
+      </button>
+    );
+  }
+);
 
-export default withTooltip(IconButton);
+export default withTooltip<IconButtonProps>(IconButton);
