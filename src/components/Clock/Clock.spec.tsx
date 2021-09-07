1fc932f90d230bde74c63b86d7180b749c4b4708
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-09-07 13:24:44 +0200

    onChange tests

--- src/components/Clock/Clock.spec.tsx
+++ src/components/Clock/Clock.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 // import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 import Clock from "./Clock";
 
 function getHourPicker(container: Element): Element {
@@ -22,6 +23,22 @@ function getSecondPicker(container: Element): Element {
   return timeValues[2];
 }
 
+function getHours(container: Element): Element[] {
+  return Array.from(
+    getHourPicker(container).querySelectorAll(".rc-clock__picker__item")
+  );
+}
+function getMinutes(container: Element): Element[] {
+  return Array.from(
+    getMinutePicker(container).querySelectorAll(".rc-clock__picker__item")
+  );
+}
+function getSeconds(container: Element): Element[] {
+  return Array.from(
+    getSecondPicker(container).querySelectorAll(".rc-clock__picker__item")
+  );
+}
+
 function getSelectedHour(container: Element): Element | null {
   return getHourPicker(container).querySelector(
     ".rc-clock__picker__item--selected"
@@ -40,7 +57,9 @@ function getSelectedSecond(container: Element): Element | null {
 
 describe("tests the Clock", () => {
   it("renders the Clock", () => {
-    const { container } = render(<Clock hour={9} minute={10} second={11} />);
+    const { container } = render(
+      <Clock hour={9} minute={10} second={11} onChange={jest.fn()} />
+    );
     expect(container.querySelector(".rc-clock")).toBeTruthy();
     expect(container.querySelectorAll(".rc-clock__picker")).toHaveLength(3);
     expect(container.querySelectorAll(".rc-clock__picker__item")).toHaveLength(
@@ -49,17 +68,44 @@ describe("tests the Clock", () => {
   });
 
   it("Sets the correct hour prop", () => {
-    const { container } = render(<Clock hour={10} />);
+    const { container } = render(<Clock hour={10} onChange={jest.fn()} />);
     expect(getSelectedHour(container)?.textContent).toBe("10");
   });
 
   it("Sets the correct minute prop", () => {
-    const { container } = render(<Clock minute={10} />);
+    const { container } = render(<Clock minute={10} onChange={jest.fn()} />);
     expect(getSelectedMinute(container)?.textContent).toBe("10");
   });
 
   it("Sets the correct second prop", () => {
-    const { container } = render(<Clock second={10} />);
+    const { container } = render(<Clock second={10} onChange={jest.fn()} />);
     expect(getSelectedSecond(container)?.textContent).toBe("10");
+  });
+
+  it("Triggers onChange when clicking an hour", () => {
+    const mockOnChange = jest.fn();
+    const { container } = render(<Clock hour={10} onChange={mockOnChange} />);
+    const [, h1] = getHours(container);
+    userEvent.click(h1);
+    expect(getSelectedHour(container)?.textContent).toBe("01");
+    expect(mockOnChange).toHaveBeenCalledWith(1, 0, 0);
+  });
+
+  it("Triggers onChange when clicking an minute", () => {
+    const mockOnChange = jest.fn();
+    const { container } = render(<Clock minute={10} onChange={mockOnChange} />);
+    const [, m1] = getMinutes(container);
+    userEvent.click(m1);
+    expect(getSelectedMinute(container)?.textContent).toBe("01");
+    expect(mockOnChange).toHaveBeenCalledWith(0, 1, 0);
+  });
+
+  it("Triggers onChange when clicking an second", () => {
+    const mockOnChange = jest.fn();
+    const { container } = render(<Clock second={10} onChange={mockOnChange} />);
+    const [, s1] = getSeconds(container);
+    userEvent.click(s1);
+    expect(getSelectedSecond(container)?.textContent).toBe("01");
+    expect(mockOnChange).toHaveBeenCalledWith(0, 0, 1);
   });
 });
